6bee75615cf208a495372e0f47a925b0a1784787
yueshuai1992 <297996582@qq.com>
2016-04-26 20:22:10 +0800

    2022

---
+++ public/js/order.js
@@ -0,0 +1,31 @@
+// var oSpan=document.getElementById("span-2");
+// var iSpan=document.getElementById("span-3");
+// var pSpan=document.getElementById("span-4");
+// oSpan.onclick = function(){
+// 	var val = iSpan.value;
+// 	if(val>0){
+// 		val--;
+// 	}else{
+// 		val==0;
+// 	}
+// 	iSpan.value=val;
+// }
+// <div class="rute">
+// 	<div class="rute-1"></div>
+// 	<div class="rute-2">1</div>
+// 	<div class="rute-3"></div>
+// 	<div class="rute-4">2</div>
+// 	<div class="rute-5"></div>
+// 	<div class="rute-6">3</div>
+// 	<div class="rute-7"></div>
+// 	<div class="rute-8">4</div>
+// 	<div class="rute-9"></div>
+// </div>
+// <div class="text">
+// 	<ul class="text-1">
+// 		<li class="text-2">选择路线</li>
+// 		<li class="text-3">填写订单</li>
+// 		<li class="text-4">在线支付</li>
+// 		<li class="text-5">付款成功</li>
+// 	</ul>
+// </div>
