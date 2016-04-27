8ba47b39aeb958e53f72d430b95af031a0d03d17
yueshuai1992 <297996582@qq.com>
2016-04-27 22:30:49 +0800

    2230

--- sever.js
+++ sever.js
@@ -1,8 +1,92 @@
-var express = require("express");
 var fs = require("fs");
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 app.use(express.static("public"));
-app.get("/index自驾团在线支付页面.html",function(req,res){
+app.use(bodyParser.urlencoded({extended:false}));
+app.get("/index自驾团主页.html",function(req,res){
 	res.send("hello")
 })
+app.post("/Email",function(req,res){
+	var obj = req.body;
+	fs.readFile("data/userlist.json","utf8",function(err,data1){
+		var result;
+		var userlist = JSON.parse(data1);
+		for(var i in userlist){
+			if(userlist[i].Email==obj.Email1){
+				result=true;
+				break;
+			}else
+			{
+				result=false;
+			}
+		}
+		res.send(result)
+	})
+})
+app.post("/username",function(req,res){
+	var obj = req.body;
+	fs.readFile("data/userlist.json","utf8",function(err,data1){
+		var result;
+		var userlist = JSON.parse(data1);
+		for(var i in userlist){
+			if(userlist[i].username==obj.username1){
+				result=true;
+				break;
+			}else
+			{
+				result=false;
+			}
+		}
+		res.send(result)
+	})
+})
+app.post("/massage",function(req,res){
+	var obj =req.body;
+	fs.readFile("data/userlist.json","utf8",function(err,data1){
+		var userlist = JSON.parse(data1);
+		userlist.push(obj);
+		userlistStr = JSON.stringify(userlist,null,4)
+		fs.writeFile("data/userlist.json",userlistStr)
+	})
+})
+app.post("/loginName",function(req,res){
+	var obj =req.body;
+	fs.readFile("data/userlist.json","utf8",function(err,data1){
+		var userlist = JSON.parse(data1);
+		var result;//布尔
+
+		for(var i in userlist){
+			if(userlist[i].Email==obj.loginname||userlist[i].username==obj.loginname){
+				result=true;
+				break;
+			}else{
+				result=false;
+			}
+		}
+		res.send(result)
+	});
+});
+app.post("/loginPass",function(req,res){
+	var obj = req.body;
+	fs.readFile("data/userlist.json","utf8",function(err,data1){
+		var userlist = JSON.parse(data1);
+		var result;//布尔
+		for(var i in userlist){
+			if(userlist[i].Email==obj.username||userlist[i].username==obj.username){
+				if(userlist[i].password==obj.password){
+					result=true;
+					break;
+				}else{
+					result=false;
+				}
+			}else{
+				result=false;
+			}
+		}
+		res.send(result)
+	});
+});
+
 app.listen(3000)
+console.log("服务器启动！")
